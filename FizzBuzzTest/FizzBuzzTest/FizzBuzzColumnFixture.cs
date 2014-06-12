215a73555932e19465cb7a6d3e10fe3d56fa8b7e
chavp <my.parinya@gmail.com>
2014-06-12 11:57:13 +0700

    First Push
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ FizzBuzzTest/FizzBuzzTest/FizzBuzzColumnFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzzTest
+{
+    using fit;
+
+    public class FizzBuzzColumnFixture : ColumnFixture
+    {
+        // source -> http://en.wikipedia.org/wiki/Fizz_buzz
+
+        public int Number { get; set; }
+
+        public string FizzBuzzSay()
+        {
+            if (Number % 3 == 0) return "Fizz";
+            if (Number % 5 == 0) return "Buzz";
+            return Number.ToString();
+        }
+    }
+}
---
+++ FizzBuzzTest/FizzBuzzTest/Timesheet.cs
@@ -0,0 +1,29 @@
+﻿using fit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzzTest
+{
+    public class Timesheet : ColumnFixture
+    {
+        public DateTime CheckInTime { get; set; }
+        public DateTime CheckOutTime { get; set; }
+        public double Rate { get; set; }
+
+        public double Pay()
+        {
+            return (Rate * HoursWorked());
+        }
+
+        // returns the number of hours worked
+        public double HoursWorked()
+        {
+            TimeSpan t = CheckOutTime.Subtract(CheckInTime);
+            double s = ((double)t.Hours) + ((double)t.Minutes / 100);
+            return s;
+        }
+
+    }
+}
